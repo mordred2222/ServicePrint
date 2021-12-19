13a2502209d5d2924dc5495b38393092d3b2df9e
Roland Van Puyenbroeck <5094960+mordred2222@users.noreply.github.com>
2021-12-19 11:48:35 +0100

    Add Sample PDF
    Add Sample PDF

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-import time
-import pandas as pa
-import re
+#import time
+#import pandas as pa
+#import re
 import os
+import PyPDF2
+#path='/path/to/process/'
+path='./folders_with_pdf/'
+
+files=os.listdir(path)
+for file in files: 
+        print (file)
+        try:
+            PyPDF2.PdfFileReader(open(path+file, "rb"))
+        except PyPDF2.utils.PdfReadError:
+                print("invalid PDF file")
+        else :
+                pass
